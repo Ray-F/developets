6542423cb367388e1410c0274e390c7dd2d4d4cf
Raymond Feng <rf.raymondfeng@gmail.com>
2021-07-04 04:05:55 +1200

    Add Event route/controller schematic

--- server/src/controller/EventController.ts
+++ server/src/controller/EventController.ts
@@ -1,8 +1,34 @@
+import { PetRepository } from '../repository/PetRepository';
+import { mongoService } from '../model/mongodb/MongoService';
+
 
 const githubEvent = async (req, res) => {
-  console.log(githubEvent)
-}
+  const petRepo = new PetRepository(mongoService);
+
+  // Positive number corresponding to number of HP deducted (cannot kill pet)
+  const linearPenalty = parseInt('10');
+
+  if (linearPenalty === 0) {
+    res.json({
+               status: 200,
+               message: 'No bad practice found!',
+             });
+  } else {
+    let { coin, hp } = await petRepo.getCoinAndHp();
+
+    // Determine what the lowest punishment is (based on either halving HP or the linear penalty)
+    const actualPenalty = Math.min(Math.round(hp / 2), linearPenalty);
+
+    await petRepo.setCoinAndHp(coin, hp - actualPenalty);
+
+    res.json({
+               status: 200,
+               message: `Deducted ${actualPenalty} health points for the bad practice!`,
+             });
+  }
+
+};
 
 export default {
-
-}
+  githubEvent,
+};
