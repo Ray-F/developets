41c4ffbc2c76c9a863791c14cf1a212a21484d43
Raymond Feng <rf.raymondfeng@gmail.com>
2021-07-04 13:25:35 +1200

    Add regex

--- server/src/controller/EventController.ts
+++ server/src/controller/EventController.ts
@@ -6,28 +6,30 @@ const githubEvent = async (req, res) => {
   const petRepo = new PetRepository(mongoService);
 
   console.log(req.body);
+  const commitMsg = req.body['head_commit']['message'];
 
-  // Positive number corresponding to number of HP deducted (cannot kill pet)
-  const linearPenalty = parseInt('0');
+  const regex = /DEV-\d+/;
 
-  if (linearPenalty === 0) {
+  if (commitMsg.search(regex) != -1) {
     res.json({
                status: 200,
                message: 'No bad practice found!',
              });
-  } else {
-    let { coin, hp } = await petRepo.getCoinAndHp();
-
-    // Determine what the lowest punishment is (based on either halving HP or the linear penalty)
-    const actualPenalty = Math.min(Math.round(hp / 2), linearPenalty);
-
-    await petRepo.setCoinAndHp(coin, hp - actualPenalty);
-
-    res.json({
-               status: 200,
-               message: `Deducted ${actualPenalty} health points for the bad practice!`,
-             });
+    return;
   }
+
+
+  let { coin, hp } = await petRepo.getCoinAndHp();
+
+  // Determine what the lowest punishment is (based on either halving HP or the linear penalty)
+  const actualPenalty = Math.min(Math.round(hp / 2), 10);
+
+  await petRepo.setCoinAndHp(coin, Math.round(hp) === 1 ? 1 : hp - actualPenalty);
+
+  res.json({
+             status: 200,
+             message: `Deducted ${actualPenalty} health points for the bad practice!`,
+           });
 
 };
 
